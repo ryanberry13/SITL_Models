dacb7fa0af6e7595283ede7f7615a4dfaa78b18f
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-09-03 00:17:11 +0100

    CameraTracking: compound transforms
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- Gazebo/scripts/omnirover_pose.py
+++ Gazebo/scripts/omnirover_pose.py
@@ -37,6 +37,7 @@ python ./src/ardupilot_sitl_models/Gazebo/scripts/omnirover_pose.py
 
 """
 
+import numpy as np
 import time
 import unittest
 
@@ -50,17 +51,31 @@ from gz.msgs10.model_pb2 import Model
 from gz.transport13 import Node
 from gz.transport13 import SubscribeOptions
 
+from transforms3d import affines
+from transforms3d import quaternions
+
 
 mutex = Lock()
 
 
 class Pose:
-    def __init__(self):
-        self.name = ""
-        self.frame_id = ""
-        self.child_frame_id = ""
-        self.children = {}
-        self.parents = {}
+    def __init__(self, world, body, position, orientation):
+        self.world = world
+        self.body = body
+        self.position = position
+        self.orientation = orientation
+
+    def __str__(self):
+        s = (
+            "world: " + str(self.world)
+            + "\n"
+            + "body: " + str(self.body)
+            + "\n"
+            + "position: " + str(self.position)
+            + "\n"
+            + "orientation: " + str(self.orientation)
+        )
+        return s
 
 
 class PoseMonitor:
@@ -76,21 +91,19 @@ class PoseMonitor:
         self._node = Node()
 
         # poses
-        self._target_pose_msg = None
-        self._target_pose_topic = f"/model/{TARGET_NAME}/pose"
-        self._target_pose_sub = self._node.subscribe(
-            Pose_V, self._target_pose_topic, self.target_pose_cb
+        self._target_pose_v_msg = None
+        self._target_pose_v_topic = f"/model/{TARGET_NAME}/pose"
+        self._target_pose_v_sub = self._node.subscribe(
+            Pose_V, self._target_pose_v_topic, self.target_pose_v_cb
         )
-        self._target_poses = {}
-        # print(self._target_pose_sub)
+        # print(self._target_pose_v_sub)
 
-        self._camera_print_msg = True
-        self._camera_pose_msg = None
-        self._camera_pose_topic = f"/model/{CAMERA_NAME}/pose"
-        self._camera_pose_sub = self._node.subscribe(
-            Pose_V, self._camera_pose_topic, self.camera_pose_cb
+        self._camera_do_print_msg = True
+        self._camera_pose_v_msg = None
+        self._camera_pose_v_topic = f"/model/{CAMERA_NAME}/pose"
+        self._camera_pose_v_sub = self._node.subscribe(
+            Pose_V, self._camera_pose_v_topic, self.camera_pose_v_cb
         )
-        self._camera_poses = {}
         # print(self._camera_pose_sub)
 
         # joint states (model)
@@ -117,22 +130,22 @@ class PoseMonitor:
         self._update_thread = Thread(target=self.update)
         self._update_thread.run()
 
-    def target_pose_cb(self, msg: Pose_V):
+    def target_pose_v_cb(self, msg: Pose_V):
         with mutex:
-            self._target_pose_msg = msg
+            self._target_pose_v_msg = msg
 
-        pose = self._target_pose_msg.pose[0]
+        pose = self._target_pose_v_msg.pose[0]
         # print("target pose")
         # print(pose.position)
 
-    def camera_pose_cb(self, msg: Pose_V):
+    def camera_pose_v_cb(self, msg: Pose_V):
         with mutex:
-            self._camera_pose_msg = msg
+            self._camera_pose_v_msg = msg
 
-        if self._camera_print_msg:
-            self._camera_print_msg = False
+        if self._camera_do_print_msg:
+            self._camera_do_print_msg = False
             print("camera pose")
-            for pose in self._camera_pose_msg.pose:
+            for pose in self._camera_pose_v_msg.pose:
                 print(pose.name)
 
     # def target_model_cb(self, msg: Model):
@@ -159,39 +172,77 @@ class PoseMonitor:
     def update(self):
         update_rate = 1.0
         update_period = 1.0 / update_rate
-        # count = 0
         while True:
-            # print(f"count: {count}")
-            # count += 1
-
-            if self._camera_pose_msg is not None:
-                # create dictionaries for camera pose
-                self._camera_poses.clear()
-                for pose in self._camera_pose_msg.pose:
-                    self._camera_poses[pose.name] = pose
-
-                # determine transform chain for
-                # mount::gimbal::pitch_link::camera -> world (=playpen)
+            if self._camera_pose_v_msg is not None:
                 world = "playpen"
-                source = "mount::gimbal::pitch_link::camera"
-
-                child = source
-                print(f"{child}")
-                parent = None
-                while parent != world:
-                    pose = self._camera_poses[child]
-                    for data in pose.header.data:
-                        if data.key == "frame_id":
-                            parent = data.value[0]
-                            break
-
-                    # print(f"child:  {child}")
-                    # print(f"parent: {parent}\n")
-                    print(f"--> {child}")
-                    child = parent
-                print(f"--> {parent}\n")
+                body = "mount::gimbal::pitch_link::camera"
+                self.body_to_world(self._camera_pose_v_msg, world, body)
 
             time.sleep(update_period)
+
+    def body_to_world(self, pose_v_msg, world, body):
+        """
+        Calculate the transform from body to world from a pose_v_msg
+        """
+        # create dictionaries for the poses
+        pose_dict = {}
+        for pose in pose_v_msg.pose:
+            pose_dict[pose.name] = pose
+
+        child = body
+        print(f"{child}")
+        parent = None
+        X = np.identity(4)
+        # print(X)
+        while parent != world:
+            pose = pose_dict[child]
+
+            for data in pose.header.data:
+                if data.key == "frame_id":
+                    parent = data.value[0]
+                    break
+
+            # print(f"child:  {child}")
+            # print(f"parent: {parent}\n")
+            print(f"--> {child}")
+            child = parent
+
+            # convert pose to an affine transform
+            X_p_c = np.array(self.pose_to_tf(pose))
+            X = np.matmul(X_p_c, X)
+            # print(X_p_c)
+            # print(X)
+
+        print(f"--> {parent}")
+
+        t, r, z, s = affines.decompose(X)
+        q = quaternions.mat2quat(r)
+        # print(f"t: {t}")
+        # print(f"q: {q}")
+
+        pose = Pose(world, body, t, q)
+        print(pose)
+        print()
+
+    def pose_to_tf(self, pose):
+        """
+        Convert a pose message into an transforms3d.affine
+        """
+        t = [pose.position.x, pose.position.y, pose.position.z]
+        q = [
+            pose.orientation.w,
+            pose.orientation.x,
+            pose.orientation.y,
+            pose.orientation.z,
+        ]
+        r = quaternions.quat2mat(q)
+        z = [1.0, 1.0, 1.0]
+        a = affines.compose(t, r, z)
+        # print(f"p: {t}")
+        # print(f"q: {q}")
+        # print(f"r: {r}")
+        # print(f"a: {a}")
+        return a
 
 
 def main():
