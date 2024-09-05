8bd72aded2ea8e39ecf5b4e139f3cb1f8a1d8774
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-09-05 15:36:34 +0100

    CameraTracking: stub out mavlink commands in wx_object_tracker
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- Gazebo/scripts/wx_object_tracker.py
+++ Gazebo/scripts/wx_object_tracker.py
@@ -9,18 +9,34 @@ Video class to capture GStreamer frames
 CameraPanel class to display openCV images in wxWidgets
   https://stackoverflow.com/questions/14804741/opencv-integration-with-wxpython
 
+
+MAVLink Camera Commands
+  https://ardupilot.org/dev/docs/mavlink-camera.html#mavlink-camera
 """
 
 import copy
 import cv2
 import gi
 import numpy as np
+import threading
+import time
 import wx
 
+from pymavlink import mavutil
 from ultralytics import YOLO
 
 gi.require_version("Gst", "1.0")
 from gi.repository import Gst
+
+ONBOARD_CONTROLLER_SYSID = 245
+ONBOARD_CONTROLLER_COMPID = 0
+IP = "127.0.0.1"
+PORT = 14550
+
+# Globals
+connection = None
+
+# GStreamer video capture
 
 
 class Video:
@@ -156,11 +172,18 @@ class Video:
         return Gst.FlowReturn.OK
 
 
+# Wx camera panel
+
+
 class CameraPanel(wx.Panel):
     def __init__(self, parent, video, fps=30):
         wx.Panel.__init__(self, parent)
 
         self._video = video
+
+        # Shared between threads
+        self._frame_lock = threading.Lock()
+        self._latest_frame = None
 
         print("Initialising stream...")
         waited = 0
@@ -235,13 +258,172 @@ class CameraPanel(wx.Panel):
             self.Refresh()
 
 
+# MAVLink commands
+
+
+# GCS -> FC
+def connect_to_mavlink(ip, port):
+    """Establish a MAVLink connection."""
+    connection = mavutil.mavlink_connection(
+        f"udp:{ip}:{port}", source_system=ONBOARD_CONTROLLER_SYSID
+    )
+    connection.wait_heartbeat()
+
+    print(
+        "Heartbeat received: system {}: component {}".format(
+            connection.target_system, connection.target_component
+        )
+    )
+
+    # Send a heartbeat back to register the device
+    connection.mav.heartbeat_send(
+        type=mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
+        autopilot=mavutil.mavlink.MAV_AUTOPILOT_INVALID,
+        base_mode=0,
+        custom_mode=0,
+        system_status=mavutil.mavlink.MAV_STATE_UNINIT,
+        mavlink_version=3,
+    )
+    return connection
+
+
+# GCS -> FC
+def send_camera_information(connection):
+    """Send the camera information."""
+    # Current time in milliseconds since boot
+    time_boot_ms = int(time.time() * 1000) & 0xFFFFFFFF
+
+    # Dummy data for camera information
+    vendor_name = b"Gazebo" + b"\0" * (32 - len("Gazebo"))
+    model_name = b"RGB" + b"\0" * (32 - len("RGB"))
+    # Example: version 1.0.0.1
+    firmware_version = (1 << 24) | (0 << 16) | (0 << 8) | 1
+    focal_length = float("nan")  # Not known
+    sensor_size_h = float("nan")  # Not known
+    sensor_size_v = float("nan")  # Not known
+    resolution_h = 640  # Example resolution
+    resolution_v = 480  # Example resolution
+    lens_id = 0  # Not known
+    flags = 4095  # All capabilities
+    cam_definition_version = 0  # Not known
+    cam_definition_uri = b""  # Not known
+    gimbal_device_id = 0  # Not known
+
+    # Send camera information
+    connection.mav.camera_information_send(
+        time_boot_ms,
+        vendor_name,
+        model_name,
+        firmware_version,
+        focal_length,
+        sensor_size_h,
+        sensor_size_v,
+        resolution_h,
+        resolution_v,
+        lens_id,
+        flags,
+        cam_definition_version,
+        cam_definition_uri,
+        gimbal_device_id,
+    )
+    print("Sent camera information")
+
+
+# GCS -> FC
+def send_cmd_camera_track_rectangle(connection):
+    """Send a camera track rectangle command."""
+    pass
+
+
+# GCS -> FC
+def send_cmd_camera_stop_tracking(connection):
+    """Send a camera stop tracking command."""
+    pass
+
+
+def handle_camera_track_point(msg):
+    print("Received MAV_CMD_CAMERA_TRACK_POINT")
+    x1 = msg.param1
+    y1 = msg.param2
+    print("Tracking point ({}, {}): ".format(x1, y1))
+
+
+def handle_camera_track_rectangle(msg):
+    print("Received MAV_CMD_CAMERA_TRACK_RECTANGLE")
+    x1 = msg.param1
+    y1 = msg.param2
+    x2 = msg.param1
+    y2 = msg.param2
+    print("Tracking rectangle ({}, {}), ({}, {}): ".format(x1, y1, x2, y2))
+
+
+# Companion -> FC
+def send_gimbal_manager_pitch_yaw_angles(connection):
+    """Send pitch and yaw angles to the gimbal manager."""
+    pass
+
+
+def control_gimbal():
+    global connection
+
+    print("Started gimbal control thread")
+
+    while True:
+        msg = connection.recv_match(type="COMMAND_LONG", blocking=False)
+        if not (msg and msg.get_type() == "COMMAND_LONG"):
+            continue
+
+        if not (msg.target_system == ONBOARD_CONTROLLER_SYSID):
+            print("Ignoring camera command for system: {}".format(msg.target_system))
+            continue
+
+        if msg.command == mavutil.mavlink.MAV_CMD_CAMERA_TRACK_POINT:
+            handle_camera_track_point(msg)
+        elif msg.command == mavutil.mavlink.MAV_CMD_CAMERA_TRACK_RECTANGLE:
+            handle_camera_track_rectangle(msg)
+
+
 def main():
+    global connection
+
+    # Establish mavlink connection
+    connection = connect_to_mavlink(IP, PORT)
+
+    # Start threads
+    control_thread = threading.Thread(target=control_gimbal, name="control")
+    control_thread.start()
+
+    # Send camera information and start tracking
+    send_camera_information(connection)
+    send_cmd_camera_track_rectangle(connection)
+
+    # GCS -> FC: MAV_CMD_CAMERA_TRACK_RECTANGLE
+    #
+    # AP_Camera::handle_command
+    #   AP_Camera::set_tracking
+    #     AP_Camera_Backend::set_tracking
+    #       AP_Camera_Backend::set_tracking_external
+    #
+    # FC -> Companion: MAV_CMD_CAMERA_TRACK_RECTANGLE
+    #
+    #         AP_Camera_Tracking::set_tracking
+    #           GCS_MAVLINK::send_message
+    #             MAVLINK_MSG_ID_COMMAND_LONG
+    #               MAV_CMD_CAMERA_TRACK_RECTANGLE
+    #               params1 = x1
+    #               params2 = y1
+    #               params3 = x2
+    #               params4 = y2
+    #
+    # Companion -> FC: GIMBAL_MANAGER_SET_PITCHYAW
+
+    # App must run on the main thread
     app = wx.App()
     wx_frame = wx.Frame(None)
 
     # Create the video and camera panel
     video = Video(port=5600)
-    camera_panel = CameraPanel(wx_frame, video, fps=30)
+    # camera_panel = CameraPanel(wx_frame, video, fps=30)
 
     wx_frame.Show()
     app.MainLoop()
