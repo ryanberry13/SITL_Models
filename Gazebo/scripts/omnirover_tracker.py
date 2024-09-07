0a25bc314fa156da8f98b0bc23ba3ecc8a2fc618
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-09-07 23:04:23 +0100

    CameraTracking: add gimbal control from GSoC project
    - GimbalControl class from ardupilot/libraries/AP_Camera/examples/tracking.py
    - Replace hardcoded height and width with variables
    - Replace hardcoded update period with constant
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- Gazebo/scripts/omnirover_tracker.py
+++ Gazebo/scripts/omnirover_tracker.py
@@ -77,6 +77,7 @@ conflicting with the output from the `PosePublisher`
 import copy
 import cv2
 import gi
+import math
 import numpy as np
 import threading
 import time
@@ -86,6 +87,8 @@ from gz.msgs10.camera_info_pb2 import CameraInfo
 from gz.msgs10.pose_v_pb2 import Pose_V
 
 from gz.transport13 import Node
+
+from pymavlink import mavutil
 
 from transforms3d import affines
 from transforms3d import euler
@@ -230,11 +233,12 @@ class Video:
 
 
 class CameraPanel(wx.Panel):
-    def __init__(self, parent, video, camera_target_tracker, fps=30):
+    def __init__(self, parent, video, camera_target_tracker, gimbal_control, fps=30):
         wx.Panel.__init__(self, parent)
 
         self._video = video
         self._camera_target_tracker = camera_target_tracker
+        self._gimbal_control = gimbal_control
 
         # Shared between threads
         self._frame_lock = threading.Lock()
@@ -288,6 +292,9 @@ class CameraPanel(wx.Panel):
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.bmp.CopyFromBuffer(frame)
             self.Refresh()
+
+            # gimbal control - probably does not need to be in the app?
+            self._gimbal_control.update_center(u, v)
 
 
 class Pose:
@@ -625,7 +632,7 @@ class CameraTargetTracker:
         #   camera sensor frame: FRU
         #   camera optical frame: RDF
         t = [0.0, 0.0, 0.0]
-        q = euler.euler2quat(np.radians(-90.0), 0.0, np.radians(-90.0))
+        q = euler.euler2quat(math.radians(-90.0), 0.0, math.radians(-90.0))
         r = quaternions.quat2mat(q)
         z = [1.0, 1.0, 1.0]
         X_c_o = affines.compose(t, r, z)
@@ -658,6 +665,74 @@ class CameraTargetTracker:
         return [u, v]
 
 
+# from ardupilot/libraries/AP_Camera/examples/tracking.py
+#
+# modifications
+#   - replace hardcoded height and width with variables
+#   - replace hardcoded update period with constant
+class GimbalControl:
+    def __init__(self, connection_str):
+        self.master = mavutil.mavlink_connection(connection_str)
+        self.master.wait_heartbeat()
+        print(
+            "Heartbeat from the system (system %u component %u)"
+            % (self.master.target_system, self.master.target_component)
+        )
+        self.center_x = 0
+        self.center_y = 0
+        self.lock = threading.Lock()  # Initialize a lock for shared variables
+        self.control_thread = threading.Thread(target=self.send_command)
+        self.control_thread.start()
+
+        self._width = 640
+        self._height = 480
+
+    def send_gimbal_manager_pitch_yaw_angles(self, pitch, yaw, pitch_rate, yaw_rate):
+        msg = self.master.mav.gimbal_manager_set_pitchyaw_encode(
+            self.master.target_system,
+            self.master.target_component,
+            0,
+            0,
+            pitch,
+            yaw,
+            pitch_rate,
+            yaw_rate,
+        )
+        self.master.mav.send(msg)
+
+    def send_command(self):
+        while True:
+            start_time = time.time()  # Record the start time of the loop
+
+            with self.lock:  # Lock when accessing shared variables
+                centre_x_copy = int(self.center_x)
+                centre_y_copy = int(self.center_y)
+
+            if centre_x_copy == 0 and centre_y_copy == 0:
+                diff_x = 0
+                diff_y = 0
+            else:
+                diff_x = (centre_x_copy - (self._width / 2)) / 2
+                diff_y = -(centre_y_copy - (self._height / 2)) / 2
+
+            self.send_gimbal_manager_pitch_yaw_angles(
+                float("NaN"), float("NaN"), math.radians(diff_y), math.radians(diff_x)
+            )
+
+            # 50Hz
+            update_period = 0.02
+            elapsed_time = time.time() - start_time
+            sleep_time = max(
+                0, update_period - elapsed_time
+            )  # Ensure no negative sleep time
+            time.sleep(sleep_time)
+
+    def update_center(self, x, y):
+        with self.lock:  # Lock when updating shared variables
+            self.center_x = x
+            self.center_y = y
+
+
 def main():
     # create an object tracker
     camera_target_tracker = CameraTargetTracker()
@@ -665,12 +740,17 @@ def main():
     # create the video object
     video = Video(port=5600)
 
+    # gimbal controller (change UDP port to 14550)
+    gimbal_control = GimbalControl("127.0.0.1:14550")
+
     # app must run on the main thread
     app = wx.App()
     wx_frame = wx.Frame(None)
 
     # create the camera panel
-    camera_panel = CameraPanel(wx_frame, video, camera_target_tracker, fps=30)
+    camera_panel = CameraPanel(
+        wx_frame, video, camera_target_tracker, gimbal_control, fps=30
+    )
 
     wx_frame.Show()
     app.MainLoop()
